dce87382d523cfc45e1521c1b8ff80583dbb050c
swapnil <swapnilthorat2560@gmail.com>
2023-04-30 17:33:16 +0530

    first commit

--- amazon.js
+++ amazon.js
@@ -1,4 +1,3 @@
-
 // LightDarkMode
 
 var icon = document.getElementById("icon");
@@ -9,4 +8,14 @@ icon.src="sun.png"
 }else{
 icon.src="moon.png"
 }
+}
+// Coupon
+
+function loadCoupon(){
+document.getElementById('coupon').style.visibility = 'visible';
+document.getElementById('main').style.opacity = '0.3'
+}
+function closeCoupon(){
+document.getElementById('coupon').style.visibility = 'hidden';
+document.getElementById('main').style.opacity = '3'
 }
