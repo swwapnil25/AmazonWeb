a7f52ea244440fa27d8e923fa8b081c456e6bcec
Swapnil Thorat <85275673+swwapnil25@users.noreply.github.com>
2023-07-11 13:23:53 +0530

    amazon.js

--- amazon.js
+++ amazon.js
@@ -1,16 +1,17 @@
-// LightDarkMode
-
-var icon = document.getElementById("icon");
-icon.onclick = function(){
-document.body.classList.toggle("dark-theme");
-if(document.body.classList.contains("dark-theme")){
-icon.src="sun.png"
-}else{
-icon.src="moon.png"
-}
-}
+// geolocation
+// window.onload = geolocation();
+// let x = document.getElementById('out');
+// function geolocation(){
+// if(navigator.geolocation){
+// navigator.geolocation.getCurrentPosition(showPosition)
+// }else{
+// x.innerText="Geo Not Supported"
+// }
+// }
+// function showPosition(data){
+// console.log(data)
+// }
 // Coupon
-
 function loadCoupon(){
 document.getElementById('coupon').style.visibility = 'visible';
 document.getElementById('main').style.opacity = '0.3'
@@ -19,3 +20,13 @@ function closeCoupon(){
 document.getElementById('coupon').style.visibility = 'hidden';
 document.getElementById('main').style.opacity = '3'
 }
+// LightDarkMode
+// var icon = document.getElementById("icon");
+// icon.onclick = function(){
+// document.body.classList.toggle("dark-theme");
+// if(document.body.classList.contains("dark-theme")){
+// icon.src="sun.png"
+// }else{
+// icon.src="moon.png"
+// }
+// }
